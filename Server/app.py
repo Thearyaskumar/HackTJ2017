c0b332c36578f071aa84624e96ea2bca1568dd5b
Arya Kumar <2019akumar2@tjhsst.edu>
2017-03-25 20:03:51 -0400

    Server Stuff

--- Server/app.py
+++ Server/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
-from flask.ext.pymongo import PyMongo
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 mongo = PyMongo(app)
 
 @app.route("/users")
 def users():
-	return "doStuff"
+    data = []
+    cursor = mongo.db.app.find()
+    for user in cursor:
+	data.append(user.get("Name"))
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
