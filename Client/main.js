0102ddd3634ca571ea73079f86e6580ca8fe81ab
Bejoy Sen <bejoysen.math@gmail.com>
2017-03-26 04:36:31 -0400

    Made Client Changes

--- Client/main.js
+++ Client/main.js
@@ -12,10 +12,32 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, '/views/index/index.html'),
+    pathname: path.join(__dirname, '/views/index.html'),
     protocol: 'file:',
     slashes: true
   }))
+
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+function loginWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600, frame:false})
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, '/views/login.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -29,7 +51,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', loginWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -47,3 +69,6 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
